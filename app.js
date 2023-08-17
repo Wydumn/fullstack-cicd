ff152939a6aa7fb6f549465270168e8c2813b39a
Wydumn <971703596@qq.com>
2023-08-17 19:09:45 +0800

    lint fix

--- app.js
+++ app.js
@@ -8,12 +8,12 @@ app.use(express.static('dist'));
 
 // health check after  deployment
 app.get('/health', (req, res) => {
-  res.send('ok')
-})
+  res.send('ok');
+});
 
 app.get('/version', (req, res) => {
-  res.send('1') // change this string to ensure a new version deployed
-})
+  res.send('1'); // change this string to ensure a new version deployed
+});
 
 app.listen(PORT, () => {
   console.log('server started on port 5000');
